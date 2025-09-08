4ef665fddf0adf4301a4024e1e6e3a72da53ff5b
Sarah <sarah.case@gmail.com>
2025-09-08 04:31:00 +0000

    removed commented out code

--- docs/main.js
+++ docs/main.js
@@ -1,21 +1,3 @@
-/*console.log("main.js");
-(() => {
-  console.log("Here we are!");
-  window.addEventListener("load", (event) => {
-    const multiplier = document.getElementById("multiplier");
-    const multiplicand = document.getElementById("multiplicand");
-    const product = document.getElementById("product");
-
-    const update = () => {
-      product.innerHTML =
-        parseFloat(multiplier.value) * parseFloat(multiplicand.value);
-    };
-
-    multiplier.addEventListener("input", update);
-    multiplicand.addEventListener("input", update);
-  });
-})(); */
-
 console.log("main.js");
 (() => {
   console.log("Here we are!");
